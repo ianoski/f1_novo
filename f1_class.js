f032bdd6c8a7f9c7073d0d08808d44b304656560
Gabriela <gabriela_ianoski@estudante.sesisenai.org.br>
2024-02-29 12:06:06 -0300

    Criada a classe OBJ

--- f1_class.js
+++ f1_class.js
@@ -1,1 +1,10 @@
-//thalita vai trabalhar nas classes 
+//ianoski vai trabalhar nas classes 
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.h = h
+        this.y = y
+        this.w = w
+        this.a = a
+    }
+}
